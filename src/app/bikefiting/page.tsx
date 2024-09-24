e1e5b5f107737657bb73bec073e41f62637e5ecf
Your name <valentinmor.pro@gmail.com>
2024-09-24 11:39:37 +0200

    bikefitting

--- src/app/bikefiting/page.tsx
+++ src/app/bikefiting/page.tsx
@@ -56,10 +56,71 @@ const Bikefiting = () => {
             </div>
           </div>
         </div>
-        <div className="flex lg:flex-row-reverse flex-col lg:items-start items-center justify-center lg:mt-32 ">
+
+        <div className="flex flex-col items-center justify-center lg:mt-24 mt-16">
+          <h2 className="xl:text-4xl text-violet-900 lg:text-2xl text-3xl mt-10 font-bold w-fit bg-slate-50 py-2 px-5 mb-20 rounded-full">
+            Notre processus
+          </h2>
+          <div className="w-full h-1  bg-sky-900 absolute"></div>
+          <div className="flex lg:flex-row flex-col items-center justify-around w-full space-y-10 lg:space-y-0 lg:space-x-32 text-sky-900">
+            {/* Section 1: Analyse de la Posture et des Mouvements */}
+            <div className="flex flex-col items-center justify-center w-full lg:w-1/3">
+              <h4 className="text-2xl mb-4">Analyse de la Posture</h4>
+              <div className="w-72 rounded-full h-72 overflow-hidden relative">
+                <div className="bg-violet-700 bg-opacity-30 absolute w-full h-full"></div>
+                <img
+                  src="/ana.png"
+                  alt="Analyse de la posture"
+                  className="w-full h-full object-cover"
+                />
+              </div>
+              <div className="xl:text-xl lg:text-lg text-center w-80 h-32 px-5 mb-4 mt-10">
+                Nous réalisons une analyse complète de votre posture et de vos
+                mouvements en pédalant.
+              </div>
+            </div>
+
+            {/* Section 2: Ajustement du Vélo */}
+            <div className="flex flex-col items-center justify-center w-full lg:w-1/3">
+              <h4 className="text-2xl mb-4">Ajustement du Vélo</h4>
+              <div className="w-72 rounded-full h-72 overflow-hidden relative">
+                <div className="bg-violet-700 bg-opacity-30 absolute w-full h-full"></div>
+                <img
+                  src="/ajust.webp"
+                  alt="Ajustement du Vélo"
+                  className="w-full h-full object-cover"
+                />
+              </div>
+              <div className="xl:text-xl lg:text-lg text-center w-80 h-32 px-5 mb-4 mt-10">
+                Nous ajustons la hauteur de la selle, la longueur des
+                manivelles, la position du guidon, et bien plus encore.
+              </div>
+            </div>
+
+            {/* Section 3: Conseils Personnalisés */}
+            <div className="flex flex-col items-center justify-center w-full lg:w-1/3">
+              <h4 className="text-2xl mb-4">Conseils Personnalisés</h4>
+
+              <div className="w-72 rounded-full h-72 overflow-hidden relative">
+                <div className="bg-violet-700 bg-opacity-30 absolute w-full h-full"></div>
+                <img
+                  src="/ajust.png"
+                  alt="Conseils Personnalisés"
+                  className="w-full h-full object-cover"
+                />
+              </div>
+              <div className="xl:text-xl lg:text-lg text-center w-80 h-32 px-5 mb-4 mt-10">
+                À la fin de la séance, nous vous donnons des conseils pour
+                améliorer votre position et maximiser vos performances.
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex lg:flex-row-reverse flex-col lg:items-start items-center justify-center lg:mt-40 mt-5 mb-20">
           <img
             src="bike2.png"
-            className="lg:w-100 w-60 rounded-tl-3xl rounded-br-3xl lg:mr-20 mt-20 lg:mt-0"
+            className="lg:w-100 w-60 rounded-tl-3xl rounded-br-3xl lg:mr-20 mt-20 lg:mt-0 grayscale
+          "
           ></img>
           <div className=" text-sky-900 z-20 items-center   lg:w-2/5  flex flex-col ">
             <h2 className="xl:text-3xl text-violet-900 lg:text-2xl text-3xl mb-10 lg:translate-x-36 lg:mt-10 mt-10  font-bold w-fit bg-slate-50 py-2 px-5 rounded-full  ">
