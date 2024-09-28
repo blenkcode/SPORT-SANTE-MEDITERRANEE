a85ef291c063f1f6520c6deaddd56cd449dd05f2
Your name <valentinmor.pro@gmail.com>
2024-09-28 12:40:58 +0200

    fonts

--- src/app/presta.tsx
+++ src/app/presta.tsx
@@ -1,6 +1,4 @@
 "use client";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faNotesMedical } from "@fortawesome/free-solid-svg-icons";
 
 import Link from "next/link";
 import { useState, useEffect } from "react";
